6b824d0b2bdd7eac1532a836717592df566913f5
Cangsong Wang <cwang40@slb.com>
2024-09-11 11:59:19 +0800

    remove ;

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 local nomap = vim.keymap.del
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
@@ -20,23 +20,18 @@ end, { desc = "buffer goto prev" })
 
 -- buffers
 map("n", "<leader>bn", "<cmd>enew<CR>", { desc = "buffer new" })
-
 map("n", "<leader>bx", function()
   require("nvchad.tabufline").close_buffer()
 end, { desc = "close current buffer" })
-
 map("n", "<leader>bc", function()
   require("nvchad.tabufline").closeAllBufs(false)
 end, { desc = "close other buffers" })
-
 map("n", "<leader>bl", function()
   require("nvchad.tabufline").closeBufs_at_direction ("left")
 end, { desc = "close buffers at left" })
-
 map("n", "<leader>br", function()
   require("nvchad.tabufline").closeBufs_at_direction ("right")
 end, { desc = "close buffers at right" })
-
 map("n", "<leader>bC", function()
   require("nvchad.tabufline").closeAllBufs(true)
 end, { desc = "close all buffers" })
